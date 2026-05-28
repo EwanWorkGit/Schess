3c4dcda0a922ea3a20ee5d9ffb3bb0ddc5104b26
EwanWorkGit <Ewansoderholmwork@gmail.com>
2026-05-28 21:01:48 +0200

    Add: Moveset Library, TileIsValid method. Fix: Assassin no longer stops piercing

---
+++ Assets/_Scripts/MovesetLibrary.cs
@@ -0,0 +1,291 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using UnityEngine;
+
+
+public class MovesetLibrary : MonoBehaviour
+{
+    public static MovesetLibrary Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    //MOVE OUT OF BOUNDS CHECKS TO PIECE OR BOARD
+
+    public void DefinePiece(Piece piece)
+    {
+        //DIRECTIONAL
+        if (piece.CurrentPieceType == PieceType.Bishop)
+        {
+            piece.Directions = new Vector2Int[]
+            {
+                new Vector2Int(1,1), new Vector2Int(1,-1), new Vector2Int(-1,1), new Vector2Int(-1,-1)
+            };
+
+            piece.Steps = 100;
+            piece.CurrentMoveType = MoveType.Directional;
+        }
+        else if (piece.CurrentPieceType == PieceType.King)
+        {
+            piece.Directions = new Vector2Int[]
+            {
+                new Vector2Int(1, 0), new Vector2Int(1, 1), new Vector2Int(0, 1), new Vector2Int(-1, 1),
+                new Vector2Int(-1, 0), new Vector2Int(-1, -1), new Vector2Int(0, -1), new Vector2Int(1, -1)
+            };
+
+            piece.Steps = 1;
+            piece.CurrentMoveType = MoveType.Directional;
+        }
+        else if (piece.CurrentPieceType == PieceType.Queen)
+        {
+            piece.Directions = new Vector2Int[]
+            {
+                new Vector2Int(1, 0), new Vector2Int(1, 1), new Vector2Int(0, 1), new Vector2Int(-1, 1),
+                new Vector2Int(-1, 0), new Vector2Int(-1, -1), new Vector2Int(0, -1), new Vector2Int(1, -1)
+            };
+
+            piece.Steps = 100;
+            piece.CurrentMoveType = MoveType.Directional;
+        }
+        else if (piece.CurrentPieceType == PieceType.Rook)
+        {
+            piece.Directions = new Vector2Int[]
+            {
+                new Vector2Int(1,0), new Vector2Int(0,1), new Vector2Int(-1,0), new Vector2Int(0,-1)
+            };
+
+            piece.Steps = 100;
+            piece.CurrentMoveType = MoveType.Directional;
+        }
+        else if (piece.CurrentPieceType == PieceType.Assassin)
+        {
+            piece.Directions = new Vector2Int[]
+            {
+                new Vector2Int(1,1), new Vector2Int(1,-1), new Vector2Int(-1,1), new Vector2Int(-1,-1)
+            };
+
+            piece.Steps = 4;
+            piece.CurrentMoveType = MoveType.Directional;
+        }
+
+        //POSITIONAL
+        if (piece.CurrentPieceType == PieceType.Pawn)
+        {
+            if (piece.CurrentTeam == Team.White)
+            {
+                piece.Offsets = new Vector2Int[] { new Vector2Int(0, 1) };
+            }
+            else
+            {
+                piece.Offsets = new Vector2Int[] { new Vector2Int(0, -1) };
+            }
+
+            piece.Steps = 1;
+            piece.CurrentMoveType = MoveType.Positional;
+        }
+        else if (piece.CurrentPieceType == PieceType.Knight)
+        {
+            piece.Offsets = new Vector2Int[]
+            {
+                new Vector2Int(1, 2), new Vector2Int(1, -2), new Vector2Int(-1, 2), new Vector2Int(-1, -2),
+                new Vector2Int(2,1), new Vector2Int(2, -1), new Vector2Int(-2,1), new Vector2Int(-2,-1)
+            };
+
+            piece.CurrentMoveType = MoveType.Positional;
+        }
+
+        //MIX OF BOTH
+        else if (piece.CurrentPieceType == PieceType.Artillery)
+        {
+            piece.Offsets = new Vector2Int[]
+            {
+                new Vector2Int(1,0), new Vector2Int(-1,0), new Vector2Int(0,1), new Vector2Int(0,-1)
+            };
+
+            piece.Directions = new Vector2Int[]
+            {
+                new Vector2Int(0,1) * (piece.CurrentTeam == Team.White ? 1 : -1)
+            };
+
+            piece.Steps = 100;
+        }
+    }
+    public void BaseMoves(Piece piece, BoardTile startTile, BoardManager boardManager, List<Vector2Int> validPositions)
+    {
+        if (piece.CurrentMoveType == MoveType.Directional)
+        {
+            foreach (Vector2Int dir in piece.Directions)
+            {
+                for (int i = 1; i <= piece.Steps; i++)
+                {
+                    Vector2Int pos = startTile.GridPos + (dir * i);
+
+                    //out of bounds check
+                    if (!piece.IsInsideBounds(boardManager.Tiles.GetLength(0), boardManager.Tiles.GetLength(1), pos))
+                    {
+                        break;
+                    }
+
+                    if (boardManager.Tiles[pos.x, pos.y].OccupyingPiece == null)
+                    {
+                        validPositions.Add(pos);
+                    }
+                    else
+                    {
+                        //check if enemy, then add and break
+                        if (boardManager.Tiles[pos.x, pos.y].OccupyingPiece.CurrentTeam != piece.CurrentTeam)
+                        {
+                            validPositions.Add(pos);
+                        }
+
+                        //breaks for this direction, so you cant pass occupied tiles
+                        break;
+                    }
+                }
+            }
+        }
+        else if (piece.CurrentMoveType == MoveType.Positional)
+        {
+            foreach (Vector2Int offset in piece.Offsets)
+            {
+                Vector2Int pos = startTile.GridPos + offset;
+                if (pos.x < 0 || pos.x >= boardManager.Tiles.GetLength(0) || pos.y < 0 || pos.y >= boardManager.Tiles.GetLength(1))
+                {
+                    continue;
+                }
+
+                if (boardManager.Tiles[pos.x, pos.y].OccupyingPiece == null || boardManager.Tiles[pos.x, pos.y].OccupyingPiece.CurrentTeam != piece.CurrentTeam)
+                {
+                    validPositions.Add(pos);
+                }
+            }
+        }
+    }
+    public void PawnMoves(Piece piece, BoardTile startTile, BoardManager boardManager, List<Vector2Int> validPositions)
+    {
+        foreach (Vector2Int offset in piece.Offsets)
+        {
+            //out of bounds
+            Vector2Int pos = startTile.GridPos + offset;
+            if (!piece.IsInsideBounds(boardManager.Tiles.GetLength(0), boardManager.Tiles.GetLength(1), pos))
+            {
+                continue;
+            }
+
+            //movable pieces
+            if (boardManager.Tiles[pos.x, pos.y].OccupyingPiece == null)
+            {
+                validPositions.Add(pos);
+            }
+        }
+
+        //captures
+        Vector2Int[] captureOffsets = new Vector2Int[] { new Vector2Int(1, 1), new Vector2Int(-1, 1) };
+
+        foreach (Vector2Int offset in captureOffsets)
+        {
+            Vector2Int pos = startTile.GridPos + (piece.CurrentTeam == Team.White ? offset : -offset);
+            if (piece.IsInsideBounds(boardManager.Tiles.GetLength(0), boardManager.Tiles.GetLength(1), pos) && boardManager.Tiles[pos.x, pos.y].OccupyingPiece != null)
+            {
+                validPositions.Add(pos);
+            }
+        }
+    }
+    public void AssassinMoves(Piece piece, BoardTile startTile, BoardManager boardManager, List<Vector2Int> validPositions)
+    {
+        foreach (Vector2Int dir in piece.Directions)
+        {
+            bool passedPiece = false;
+            for (int i = 1; i <= piece.Steps; i++)
+            {
+                Vector2Int pos = startTile.GridPos + (dir * i);
+
+                //out of bounds check
+                if (!piece.IsInsideBounds(boardManager.Tiles.GetLength(0), boardManager.Tiles.GetLength(1), pos))
+                {
+                    break;
+                }
+
+                if (boardManager.Tiles[pos.x, pos.y].OccupyingPiece == null)
+                {
+                    validPositions.Add(pos);
+                }
+                else
+                {
+                    //check if enemy, then add and break
+                    if (boardManager.Tiles[pos.x, pos.y].OccupyingPiece.CurrentTeam != piece.CurrentTeam)
+                    {
+                        //piece capture position
+                        if (passedPiece)
+                        {
+                            validPositions.Add(pos);
+                            passedPiece = false;
+                        }
+                        else
+                        {
+                            passedPiece = true;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    public void ArtilleryMoves(Piece piece, BoardTile startTile, BoardManager boardManager, List<Vector2Int> validPositions)
+    {
+        if (!piece.IsInActionMode)
+        {
+            foreach (Vector2Int offset in piece.Offsets)
+            {
+                Vector2Int pos = startTile.GridPos + offset;
+                //out of bounds
+                if (!piece.IsInsideBounds(boardManager.Tiles.GetLength(0), boardManager.Tiles.GetLength(1), pos) || boardManager.Tiles[pos.x, pos.y].OccupyingPiece != null)
+                {
+                    continue;
+                }
+
+                //movable pieces
+                if (boardManager.Tiles[pos.x, pos.y].OccupyingPiece == null)
+                {
+                    validPositions.Add(pos);
+                }
+            }
+        }
+        else
+        {
+            foreach (Vector2Int dir in piece.Directions)
+            {
+                for (int i = 1; i <= piece.Steps; i++)
+                {
+                    Vector2Int pos = startTile.GridPos + (dir * i);
+                    if (!piece.IsInsideBounds(boardManager.Tiles.GetLength(0), boardManager.Tiles.GetLength(1), pos))
+                    {
+                        break;
+                    }
+                    validPositions.Add(pos);
+                    float dist = Vector2Int.Distance(startTile.GridPos, pos);
+                    float distPerWidth = 3f; //how large the distance has to be for width to increase by 1
+                    int width = (int)Mathf.Floor(dist / distPerWidth);
+
+                    Vector2Int[] sideOffsets = new Vector2Int[] { new Vector2Int(1, 0), new Vector2Int(-1, 0) };
+                    foreach (Vector2Int sideOffset in sideOffsets)
+                    {
+                        for (int j = 1; j <= width; j++)
+                        {
+                            int x = pos.x + (sideOffset.x * j);
+                            Vector2Int sidePos = new Vector2Int(x, pos.y);
+                            if (!piece.IsInsideBounds(boardManager.Tiles.GetLength(0), boardManager.Tiles.GetLength(1), sidePos))
+                            {
+                                continue;
+                            }
+                            validPositions.Add(sidePos);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/_Scripts/Piece.cs
+++ Assets/_Scripts/Piece.cs
@@ -4,12 +4,12 @@ using UnityEngine.Tilemaps;
 
 public enum PieceType { Pawn, Bishop, King, Queen, Knight, Rook, Assassin, Artillery }
 
+//only for base pieces
+public enum MoveType { Directional, Positional }
+
 public class Piece : MonoBehaviour
 {
     //how pieces can move, NOT ACTUAL STATE CHANGES
-
-    //only for base pieces
-    enum MoveType { Directional, Positional }
 
     public Team CurrentTeam;
     public PieceType CurrentPieceType;    
@@ -17,15 +17,15 @@ public class Piece : MonoBehaviour
     public SpriteRenderer Renderer;
     public bool IsInActionMode = false;
     public float TurnsUntilFire;
+    public MoveType CurrentMoveType;
+    public Vector2Int[] Directions;
+    public Vector2Int[] Offsets;
 
     [SerializeField] Sprite[] SpriteArray;
     [SerializeField] Sprite DefaultSprite;
 
-    MoveType CurrentMoveType;
-    Vector2Int[] Directions;
-    Vector2Int[] Offsets;
-
-    int Steps = 1, TurnsToFire = 1;
+    public int Steps = 1;
+    float TurnsToFire = 1;
 
     private void Awake()
     {
@@ -35,11 +35,7 @@ public class Piece : MonoBehaviour
         transform.name = $"{CurrentTeam.ToString()} : {CurrentPieceType.ToString()} : {transform.position.x}x {transform.position.y}y";
     }
 
-    bool IsInsideBounds(int width, int height, Vector2Int vec)
-    {
-        bool insideBounds = vec.x >= 0 && vec.x < width && vec.y >= 0 && vec.y < height;
-        return insideBounds;
-    }
+    
 
     //moves and sprites
     public void HandleClickInteraction(ClickType clickType, Piece clickedPiece, BoardTile clickedTile)
@@ -59,13 +55,13 @@ public class Piece : MonoBehaviour
             pieceManager.DeselectPiece();
             pieceManager.SelectPiece(clickedPiece);
         }
-        else if(clickType == ClickType.Move)
+        else if(clickType == ClickType.Move && TileIsValid(pieceManager.SelectedPiece.CurrentTile, clickedTile))
         {
             boardManager.MovePieceToTile(clickedTile, pieceManager.SelectedPiece);
             turnManager.ChangeTeam(false);
             pieceManager.DeselectPiece();
         }
-        else if(clickType == ClickType.Capture)
+        else if(clickType == ClickType.Capture && TileIsValid(pieceManager.SelectedPiece.CurrentTile, clickedTile))
         {
             boardManager.CaptureOtherPiece(pieceManager.SelectedPiece, clickedPiece, clickedTile);
             boardManager.MovePieceToTile(clickedTile, pieceManager.SelectedPiece);
@@ -76,101 +72,7 @@ public class Piece : MonoBehaviour
 
     public void MoveAndSpriteConfig()
     {
-        //DIRECTIONAL
-        if (CurrentPieceType == PieceType.Bishop)
-        {
-            Directions = new Vector2Int[]
-            {
-                new Vector2Int(1,1), new Vector2Int(1,-1), new Vector2Int(-1,1), new Vector2Int(-1,-1)
-            };
-
-            Steps = 100;
-            CurrentMoveType = MoveType.Directional;
-        }
-        else if (CurrentPieceType == PieceType.King)
-        {
-            Directions = new Vector2Int[]
-            {
-                new Vector2Int(1, 0), new Vector2Int(1, 1), new Vector2Int(0, 1), new Vector2Int(-1, 1),
-                new Vector2Int(-1, 0), new Vector2Int(-1, -1), new Vector2Int(0, -1), new Vector2Int(1, -1)
-            };
-
-            Steps = 1;
-            CurrentMoveType = MoveType.Directional;
-        }
-        else if (CurrentPieceType == PieceType.Queen)
-        {
-            Directions = new Vector2Int[]
-            {
-                new Vector2Int(1, 0), new Vector2Int(1, 1), new Vector2Int(0, 1), new Vector2Int(-1, 1),
-                new Vector2Int(-1, 0), new Vector2Int(-1, -1), new Vector2Int(0, -1), new Vector2Int(1, -1)
-            };
-
-            Steps = 100;
-            CurrentMoveType = MoveType.Directional;
-        }
-        else if (CurrentPieceType == PieceType.Rook)
-        {
-            Directions = new Vector2Int[]
-            {
-                new Vector2Int(1,0), new Vector2Int(0,1), new Vector2Int(-1,0), new Vector2Int(0,-1)
-            };
-
-            Steps = 100;
-            CurrentMoveType = MoveType.Directional;
-        }
-        else if (CurrentPieceType == PieceType.Assassin)
-        {
-            Directions = new Vector2Int[]
-            {
-                new Vector2Int(1,1), new Vector2Int(1,-1), new Vector2Int(-1,1), new Vector2Int(-1,-1)
-            };
-
-            Steps = 4;
-            CurrentMoveType = MoveType.Directional;
-        }
-
-        //POSITIONAL
-        if (CurrentPieceType == PieceType.Pawn)
-        {
-            if (CurrentTeam == Team.White)
-            {
-                Offsets = new Vector2Int[] { new Vector2Int(0, 1) };
-            }
-            else
-            {
-                Offsets = new Vector2Int[] { new Vector2Int(0, -1) };
-            }
-
-            Steps = 1;
-            CurrentMoveType = MoveType.Positional;
-        }
-        else if (CurrentPieceType == PieceType.Knight)
-        {
-            Offsets = new Vector2Int[]
-            {
-                new Vector2Int(1, 2), new Vector2Int(1, -2), new Vector2Int(-1, 2), new Vector2Int(-1, -2),
-                new Vector2Int(2,1), new Vector2Int(2, -1), new Vector2Int(-2,1), new Vector2Int(-2,-1)
-            };
-
-            CurrentMoveType = MoveType.Positional;
-        }
-
-        //MIX OF BOTH
-        else if(CurrentPieceType == PieceType.Artillery)
-        {
-            Offsets = new Vector2Int[]
-            {
-                new Vector2Int(1,0), new Vector2Int(-1,0), new Vector2Int(0,1), new Vector2Int(0,-1)
-            };
-
-            Directions = new Vector2Int[]
-            {
-                new Vector2Int(0,1) * (CurrentTeam == Team.White ? 1 : -1)
-            };
-
-            Steps = 100;
-        }
+        MovesetLibrary.Instance.DefinePiece(this);
 
         int index = (int)CurrentPieceType;
         if (index < 0 || index >= SpriteArray.Length)
@@ -200,33 +102,35 @@ public class Piece : MonoBehaviour
 
         CurrentTile = closestTile;
     }
+
     
     //puts valid movesets into a list
     public Vector2Int[] GetValidTargets(BoardTile startTile)
     {
         BoardManager boardManager = BoardManager.Instance;
+        MovesetLibrary movesets = MovesetLibrary.Instance;
         List<Vector2Int> validMoves = new List<Vector2Int>();
 
         switch (CurrentPieceType)
         {
             case PieceType.Pawn:
                 {
-                    PawnMoves(startTile, boardManager, validMoves);
+                    movesets.PawnMoves(startTile.OccupyingPiece, startTile, boardManager, validMoves);
                     break;
                 }
             case PieceType.Assassin:
                 {
-                    AssassinMoves(startTile, boardManager, validMoves);
+                    movesets.AssassinMoves(startTile.OccupyingPiece, startTile, boardManager, validMoves);
                     break;
                 }
             case PieceType.Artillery:
                 {
-                    ArtilleryMoves(startTile, boardManager, validMoves);
+                    movesets.ArtilleryMoves(startTile.OccupyingPiece, startTile, boardManager, validMoves);
                     break;
                 }
             default:
                 {
-                    BaseMoves(startTile, boardManager, validMoves);
+                    movesets.BaseMoves(startTile.OccupyingPiece, startTile, boardManager, validMoves);
                     break;
                 }
         }
@@ -234,180 +138,28 @@ public class Piece : MonoBehaviour
         return validMoves.ToArray();
     }
 
-    //These manage the pieces individual movesets
-    void BaseMoves(BoardTile startTile, BoardManager boardManager, List<Vector2Int> validPositions)
+    public bool TileIsValid(BoardTile startTile, BoardTile endTile)
     {
-        if (CurrentMoveType == MoveType.Directional)
+        if(startTile == null || endTile == null)
         {
-            foreach (Vector2Int dir in Directions)
+            Debug.LogWarning($"Start or end tile null! (from {transform.name})");
+            return false;
+        }
+
+        Vector2Int[] validTiles = GetValidTargets(startTile);
+        foreach(Vector2Int tile in validTiles)
+        {
+            if(tile == endTile.GridPos)
             {
-                for (int i = 1; i <= Steps; i++)
-                {
-                    Vector2Int pos = startTile.GridPos + (dir * i);
-
-                    //out of bounds check
-                    if (!IsInsideBounds(boardManager.Tiles.GetLength(0), boardManager.Tiles.GetLength(1), pos))
-                    {
-                        break;
-                    }
-
-                    if (boardManager.Tiles[pos.x, pos.y].OccupyingPiece == null)
-                    {
-                        validPositions.Add(pos);
-                    }
-                    else
-                    {
-                        //check if enemy, then add and break
-                        if (boardManager.Tiles[pos.x, pos.y].OccupyingPiece.CurrentTeam != CurrentTeam)
-                        {
-                            validPositions.Add(pos);
-                        }
-
-                        //breaks for this direction, so you cant pass occupied tiles
-                        break;
-                    }
-                }
+                return true;
             }
         }
-        else if (CurrentMoveType == MoveType.Positional)
-        {
-            foreach (Vector2Int offset in Offsets)
-            {
-                Vector2Int pos = startTile.GridPos + offset;
-                if (pos.x < 0 || pos.x >= boardManager.Tiles.GetLength(0) || pos.y < 0 || pos.y >= boardManager.Tiles.GetLength(1))
-                {
-                    continue;
-                }
 
-                if (boardManager.Tiles[pos.x, pos.y].OccupyingPiece == null || boardManager.Tiles[pos.x, pos.y].OccupyingPiece.CurrentTeam != CurrentTeam)
-                {
-                    validPositions.Add(pos);
-                }
-            }
-        }
+        return false;
     }
-    void PawnMoves(BoardTile startTile, BoardManager boardManager, List<Vector2Int> validPositions)
+    public bool IsInsideBounds(int width, int height, Vector2Int vec)
     {
-        foreach (Vector2Int offset in Offsets)
-        {
-            //out of bounds
-            Vector2Int pos = startTile.GridPos + offset;
-            if (!IsInsideBounds(boardManager.Tiles.GetLength(0), boardManager.Tiles.GetLength(1), pos))
-            {
-                continue;
-            }
-
-            //movable pieces
-            if (boardManager.Tiles[pos.x, pos.y].OccupyingPiece == null)
-            {
-                validPositions.Add(pos);
-            }
-        }
-        
-        //captures
-        Vector2Int[] captureOffsets = new Vector2Int[] { new Vector2Int(1, 1), new Vector2Int(-1, 1) };
-
-        foreach (Vector2Int offset in captureOffsets)
-        {
-            Vector2Int pos = startTile.GridPos + (CurrentTeam == Team.White ? offset : -offset);
-            if (IsInsideBounds(boardManager.Tiles.GetLength(0), boardManager.Tiles.GetLength(1), pos) && boardManager.Tiles[pos.x, pos.y].OccupyingPiece != null)
-            {
-                validPositions.Add(pos);
-            }
-        }
-    }
-    void AssassinMoves(BoardTile startTile, BoardManager boardManager, List<Vector2Int> validPositions)
-    {
-        foreach (Vector2Int dir in Directions)
-        {
-            bool passedPiece = false;
-            for (int i = 1; i <= Steps; i++)
-            {
-                Vector2Int pos = startTile.GridPos + (dir * i);
-
-                //out of bounds check
-                if (!IsInsideBounds(boardManager.Tiles.GetLength(0), boardManager.Tiles.GetLength(1), pos))
-                {
-                    break;
-                }
-
-                if (boardManager.Tiles[pos.x, pos.y].OccupyingPiece == null)
-                {
-                    validPositions.Add(pos);
-                }
-                else
-                {
-                    //check if enemy, then add and break
-                    if (boardManager.Tiles[pos.x, pos.y].OccupyingPiece.CurrentTeam != CurrentTeam)
-                    {
-                        //piece capture position
-                        if (passedPiece)
-                        {
-                            validPositions.Add(pos);
-                            passedPiece = false;
-                        }
-                        else
-                        {
-                            passedPiece = true;
-                        }
-                    }
-                }
-            }
-        }
-        
-    }
-    void ArtilleryMoves(BoardTile startTile, BoardManager boardManager, List<Vector2Int> validPositions)
-    {
-        if(!IsInActionMode)
-        {
-            foreach (Vector2Int offset in Offsets)
-            {
-                Vector2Int pos = startTile.GridPos + offset;
-                //out of bounds
-                if (!IsInsideBounds(boardManager.Tiles.GetLength(0), boardManager.Tiles.GetLength(0), pos) || boardManager.Tiles[pos.x,pos.y].OccupyingPiece != null)
-                {
-                    continue;
-                }
-
-                //movable pieces
-                if (boardManager.Tiles[pos.x, pos.y].OccupyingPiece == null)
-                {
-                    validPositions.Add(pos);
-                }
-            }
-        }
-        else
-        {
-            foreach(Vector2Int dir in Directions)
-            {
-                for (int i = 1; i <= Steps; i++)
-                {
-                    Vector2Int pos = startTile.GridPos + (dir * i);
-                    if(!IsInsideBounds(boardManager.Tiles.GetLength(0), boardManager.Tiles.GetLength(1), pos))
-                    {
-                        break;
-                    }
-                    validPositions.Add(pos);
-                    float dist = Vector2Int.Distance(startTile.GridPos, pos);
-                    float distPerWidth = 3f; //how large the distance has to be for width to increase by 1
-                    int width = (int)Mathf.Floor(dist / distPerWidth);
-                    
-                    Vector2Int[] sideOffsets = new Vector2Int[] { new Vector2Int(1,0), new Vector2Int(-1, 0) };
-                    foreach(Vector2Int sideOffset in sideOffsets)
-                    {
-                        for (int j = 1; j <= width; j++)
-                        {
-                            int x = pos.x + (sideOffset.x * j);
-                            Vector2Int sidePos = new Vector2Int(x, pos.y);
-                            if (!IsInsideBounds(boardManager.Tiles.GetLength(0), boardManager.Tiles.GetLength(1), sidePos))
-                            {
-                                continue;
-                            }
-                            validPositions.Add(sidePos);
-                        }
-                    }
-                }
-            }
-        }        
+        bool insideBounds = vec.x >= 0 && vec.x < width && vec.y >= 0 && vec.y < height;
+        return insideBounds;
     }
 }
